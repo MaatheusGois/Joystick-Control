142eaea84377adac1a6ea02a3c94ad75543e4006
Gois <matheusgoislimasilva@gmail.com>
2019-10-09 18:16:54 -0300

    first commit

--- joystick/GameScene.swift
+++ joystick/GameScene.swift
@@ -7,83 +7,109 @@
 //
 
 import SpriteKit
-import GameplayKit
 
 class GameScene: SKScene {
     
-    private var label : SKLabelNode?
-    private var spinnyNode : SKShapeNode?
+    let joys = SKSpriteNode(imageNamed: "JOYS")
+    let base = SKSpriteNode(imageNamed: "base")
+    let nave = SKSpriteNode(imageNamed: "ship")
+    
+    var velocidadX: CGFloat = 0.0
+    var velocidadY: CGFloat = 0.0
+    
+    var joysActivo:Bool = false
     
     override func didMove(to view: SKView) {
         
-        // Get label node from scene and store it for use later
-        self.label = self.childNode(withName: "//helloLabel") as? SKLabelNode
-        if let label = self.label {
-            label.alpha = 0.0
-            label.run(SKAction.fadeIn(withDuration: 2.0))
-        }
+        backgroundColor = .white
+        anchorPoint = CGPoint(x: 0.5, y: 0.5)
         
-        // Create shape node to use during mouse interaction
-        let w = (self.size.width + self.size.height) * 0.05
-        self.spinnyNode = SKShapeNode.init(rectOf: CGSize.init(width: w, height: w), cornerRadius: w * 0.3)
+        base.position = CGPoint(x: 0 , y: 0 )
+        base.setScale(0.5)
+        base.zPosition = 1.0
+        base.alpha = 0.2
+        addChild(base)
         
-        if let spinnyNode = self.spinnyNode {
-            spinnyNode.lineWidth = 2.5
-            
-            spinnyNode.run(SKAction.repeatForever(SKAction.rotate(byAngle: CGFloat(Double.pi), duration: 1)))
-            spinnyNode.run(SKAction.sequence([SKAction.wait(forDuration: 0.5),
-                                              SKAction.fadeOut(withDuration: 0.5),
-                                              SKAction.removeFromParent()]))
-        }
+        joys.position = base.position
+        joys.setScale(0.5)
+        joys.zPosition = 2.0
+        joys.alpha = 0.2
+        joys.setScale(0.4)
+        addChild(joys)
+        
+        nave.position = CGPoint(x: self.frame.midX,y: self.frame.midY)
+        nave.xScale = 0.5
+        nave.yScale = 0.5
+        
+        nave.physicsBody = SKPhysicsBody(rectangleOf: nave.frame.size)
+        nave.physicsBody!.applyImpulse(CGVector(dx: 10, dy: 10))
+
+        self.physicsWorld.gravity = CGVector(dx: 0, dy: 0)
+        self.physicsBody = SKPhysicsBody(edgeLoopFrom:CGRect(
+            x: self.frame.minX,
+            y: self.frame.minY + nave.frame.size.height + 10,
+            width: self.frame.size.width,
+            height: (self.frame.size.height - (nave.frame.size.height + 10) * 2))
+                )
+        
+        addChild(nave)
     }
     
-    
-    func touchDown(atPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.green
-            self.addChild(n)
-        }
-    }
-    
-    func touchMoved(toPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.blue
-            self.addChild(n)
-        }
-    }
-    
-    func touchUp(atPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.red
-            self.addChild(n)
-        }
-    }
-    
+     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-        if let label = self.label {
-            label.run(SKAction.init(named: "Pulse")!, withKey: "fadeInOut")
+        for touch in touches {
+            let location = touch.location(in: self)
+            if (joys.frame.contains(location)){
+                joysActivo = true
+            } else {
+                joysActivo = false
+            }
         }
-        
-        for t in touches { self.touchDown(atPoint: t.location(in: self)) }
     }
+    
     
     override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchMoved(toPoint: t.location(in: self)) }
+        for touch in touches {
+            let location = touch.location(in: self)
+            
+            if joysActivo == true {
+            
+                let vector = CGVector(dx: location.x - base.position.x, dy: location.y - base.position.y)
+                let angulo = atan2(vector.dy, vector.dx)
+                let radio: CGFloat = base.frame.size.height / 2
+                
+                let xDist:CGFloat = sin(angulo - 1.57079633) * radio
+                let yDist:CGFloat = cos(angulo - 1.57079633) * radio
+                
+                if (base.frame.contains(location)) {
+                    joys.position = location
+                } else {
+                    joys.position = CGPoint(x: base.position.x - xDist, y: base.position.y + yDist)
+                }
+                
+                nave.zRotation = angulo - 1.57079633
+                
+                velocidadX = xDist / 49.0
+                velocidadY = yDist / 49.0
+            }
+        }
     }
     
     override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchUp(atPoint: t.location(in: self)) }
+        if joysActivo == true {
+            let retorno: SKAction = SKAction.move(to: base.position, duration: 0.05)
+            retorno.timingMode = SKActionTimingMode.easeOut
+            joys.run(retorno)
+            joysActivo = false
+            velocidadX = 0
+            velocidadY = 0
+        }
     }
-    
-    override func touchesCancelled(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchUp(atPoint: t.location(in: self)) }
-    }
-    
-    
-    override func update(_ currentTime: TimeInterval) {
-        // Called before each frame is rendered
+   
+    override func update(_ currentTime: CFTimeInterval) {
+        if joysActivo == true {
+            nave.position = CGPoint(x: nave.position.x - (velocidadX*3), y: nave.position.y + (velocidadY*3))
+        }
     }
 }
+
