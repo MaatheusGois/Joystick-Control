4847e9ec1c621b1ee75edda2a67f7c2e1343909a
Gois <matheusgoislimasilva@gmail.com>
2019-10-09 23:07:35 -0300

    Refatoracao para Joystick ser uma classe

--- joystick/GameScene.swift
+++ joystick/GameScene.swift
@@ -10,95 +10,57 @@ import SpriteKit
 
 class GameScene: SKScene {
     
-    let joys = SKSpriteNode(imageNamed: "JOYS")
-    let base = SKSpriteNode(imageNamed: "base")
     let nave = SKSpriteNode(imageNamed: "ship")
     
     var velocidadX: CGFloat = 0.0
     var velocidadY: CGFloat = 0.0
     
-    var joysActivo:Bool = false
+    var 🕹️: Joystick = Joystick(radius: 100)
     
     override func didMove(to view: SKView) {
         
         backgroundColor = .white
         
-        base.position = CGPoint(x: 0 , y: 0 )
-        base.setScale(0.5)
-        base.zPosition = 1.0
-        base.alpha = 0.2
-        addChild(base)
+        🕹️.setPosition(withLocation: CGPoint(x: 0, y: -size.height/3))
+        addChild(🕹️)
+        addChild(🕹️.child)
         
-        joys.position = base.position
-        joys.setScale(0.5)
-        joys.zPosition = 2.0
-        joys.alpha = 0.2
-        joys.setScale(0.4)
-        addChild(joys)
-        
+
+        nave.physicsBody = SKPhysicsBody(rectangleOf: nave.frame.size)
         nave.position = CGPoint(x: self.frame.midX,y: self.frame.midY)
         nave.xScale = 0.5
         nave.yScale = 0.5
         
-        nave.physicsBody = SKPhysicsBody(rectangleOf: nave.frame.size)
-        nave.physicsBody?.applyImpulse(CGVector(dx: 10, dy: 10))
-
-        self.physicsWorld.gravity = CGVector(dx: 0, dy: 0)
-        self.physicsBody = SKPhysicsBody(edgeLoopFrom: CGRect(
-            x: self.frame.minX,
-            y: self.frame.minY + nave.frame.size.height + 10,
-            width: self.frame.size.width,
-            height: (self.frame.size.height - (nave.frame.size.height + 10) * 2)))
-        
+    
         addChild(nave)
+        
+        //Deixa a gravidade valendo 0
+        self.physicsWorld.gravity = CGVector(dx: 0, dy: 0)
     }
     
-     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
         for touch in touches {
             let location = touch.location(in: self)
-            if (joys.frame.contains(location)){
-                joysActivo = true
-            } else {
-                joysActivo = false
-            }
+            🕹️.activo = 🕹️.child.frame.contains(location) ? true : false
         }
     }
-    
     
     override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
         for touch in touches {
             let location = touch.location(in: self)
-            
-            if joysActivo == true {
-            
-                let vector = CGVector(dx: location.x - base.position.x, dy: location.y - base.position.y)
-                let angulo = atan2(vector.dy, vector.dx)
-                let radio: CGFloat = base.frame.size.height / 2
+            if 🕹️.activo {
+                let dist = 🕹️.getDist(withLocation: location)
                 
-                let xDist:CGFloat = sin(angulo - 1.57079633) * radio
-                let yDist:CGFloat = cos(angulo - 1.57079633) * radio
-                
-                if (base.frame.contains(location)) {
-                    joys.position = location
-                } else {
-                    joys.position = CGPoint(x: base.position.x - xDist, y: base.position.y + yDist)
-                }
-                
-                nave.zRotation = angulo - 1.57079633
-                
-                velocidadX = xDist / 16
-                velocidadY = yDist / 16
+                nave.zRotation = 🕹️.angulo - 1.57079633
+                velocidadX = dist.xDist / 16
+                velocidadY = dist.yDist / 16
             }
         }
     }
     
     override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
-        if joysActivo == true {
-            let retorno: SKAction = SKAction.move(to: base.position, duration: 0.05)
-            retorno.timingMode = .easeOut
-            joys.run(retorno)
-            joysActivo = false
+        if 🕹️.activo {
+            🕹️.coreReturn()
             velocidadX = 0
             velocidadY = 0
         }
@@ -109,14 +71,13 @@ class GameScene: SKScene {
     
     override func update(_ currentTime: CFTimeInterval) {
         //Delta time garante que a velocidade vai ser sempre a mesma independente da velocidade do dispositivo.
-        
         deltaTime = currentTime - lastTime
         
-        
-        if joysActivo == true {
+        if 🕹️.activo {
             nave.position = CGPoint(x: nave.position.x - (velocidadX),
                                     y: nave.position.y + (velocidadY))
         }
+        
         lastTime = currentTime
     }
 }
---
+++ joystick/Joystick.swift
@@ -0,0 +1,85 @@
+//
+//  Joystick.swift
+//  joystick
+//
+//  Created by Matheus Silva on 09/10/19.
+//  Copyright © 2019 Matheus Gois. All rights reserved.
+//
+
+import Foundation
+import SpriteKit
+
+class Joystick: SKShapeNode {
+    var activo: Bool = false
+    var radius: CGFloat = 0
+    var vector: CGVector = CGVector()
+    var angulo: CGFloat = 0
+    var raio: CGFloat = 0
+    var child: SKShapeNode = SKShapeNode()
+    
+    override init() {
+        super.init()
+    }
+
+    convenience init(radius: CGFloat) {
+        self.init()
+        self.radius = radius
+        createJoystickBase()
+        createJoystickBaseMain()
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    func createJoystickBase() {
+        self.path = CGPath(ellipseIn: CGRect(origin: CGPoint(x: -radius,
+                                                             y: -radius),
+                                             size: CGSize(width: radius * 2,
+                                                          height: radius * 2)),
+                                             transform: nil)
+        self.strokeColor = .black
+        self.alpha = 0.2
+        self.lineWidth = 0.5
+        self.zPosition = 1.0
+    }
+    
+    
+    private func createJoystickBaseMain() {
+        child = SKShapeNode(circleOfRadius: radius / 2)
+        child.strokeColor = .black
+        child.alpha = 0.5
+        child.lineWidth = 1.0
+        child.zPosition = 2.0
+    }
+    
+    func getDist(withLocation location: CGPoint) -> (xDist: CGFloat, yDist: CGFloat) {
+        
+        vector = CGVector(dx: location.x - self.position.x, dy: location.y - self.position.y)
+        angulo = atan2(vector.dy, vector.dx)
+        raio = self.frame.size.height / 2
+        
+        let xDist: CGFloat = sin(angulo - 1.57079633) * raio
+        let yDist: CGFloat = cos(angulo - 1.57079633) * raio
+        
+        if (self.frame.contains(location)) {
+            self.child.position = location
+        } else {
+            self.child.position = CGPoint(x: self.position.x - xDist, y: self.position.y + yDist)
+        }
+        return (xDist: xDist, yDist: yDist)
+    }
+    
+    func coreReturn() {
+        let retorno: SKAction = SKAction.move(to: self.position, duration: 0.05)
+        retorno.timingMode = .easeOut
+        child.run(retorno)
+        activo = false
+    }
+    
+    func setPosition(withLocation location: CGPoint) {
+        self.position = location
+        self.child.position = location
+    }
+    
+}
